7d70b642a233db6fb1e13c836debc9ae12479c28
ilya <quiksilver4210@gmail.com>
2018-04-10 13:03:47 +1000

    Added new button

---
+++ constants.py
@@ -0,0 +1,14 @@
+token = "597454150:AAE7OFsWm0rNjcXVpk9LgLaYmsZjytG7mGY"
+
+import telebot
+
+user_markup_start = telebot.types.ReplyKeyboardMarkup()
+user_markup_start.row('Расписание на сегодня', 'Расписание на завтра')
+user_markup_start.row('Выбрать день недели')
+user_markup_start.row('Расписание звонков')
+
+user_markup_days = telebot.types.ReplyKeyboardMarkup()
+user_markup_days.row('Понедельник', 'Четверг')
+user_markup_days.row('Втоник', 'Пятница')
+user_markup_days.row('Среда', 'Суббота')
+user_markup_days.row('Вернуться назад')
--- main.py
+++ main.py
@@ -4,40 +4,56 @@
 ?
 """
 
-import telebot
-token = "597454150:AAE7OFsWm0rNjcXVpk9LgLaYmsZjytG7mGY"
+from datetime import date
+from _datetime import datetime
 
-bot = telebot.TeleBot(token)
+import telebot
+import calendar
+import constants
+
+bot = telebot.TeleBot(constants.token)
 
 print(bot.get_me())
+print("\n")
 
 def log(message, answer):
-    from datetime import datetime
     print(datetime.now())
-    print("Message from {0} {1}. (id = {2})\nRecived message: {3}".format(message.from_user.first_name,
-                                                                   message.from_user.last_name,
-                                                                   str(message.from_user.id),
-                                                                   message.text))
-
+    print("Message from {0} {1}. (id = {2})".format(message.from_user.first_name, message.from_user.last_name, str(message.from_user.id)))
+    print("Recieved message: {0}".format(message.text))
     print("Sended message: {0}\n".format(answer))
 
 @bot.message_handler(commands=['start'])
 def handle_start(message):
-    user_markup = telebot.types.ReplyKeyboardMarkup(True, True)
-    user_markup.row('Расписание на сегодня','Расписание на завтра')
+    bot.send_message(message.from_user.id, 'Welcome', reply_markup=constants.user_markup_start)
 
-    bot.send_message(message.from_user.id, 'Welcome', reply_markup=user_markup)
 
 @bot.message_handler(content_types=['text'])
 def handle_text(message):
+
     if message.text == "Расписание на сегодня":
-        answer = "Работоспособность 1 кнопки"
+        my_date = date.today()
+        answer = calendar.day_name[my_date.weekday()]
+        log(message,answer)
+        bot.send_message(message.chat.id,answer, reply_markup=constants.user_markup_start)
+
+    elif message.text == "Расписание на завтра":
+        my_date = date.today()
+        answer = calendar.day_name[my_date.weekday()+1]
+        bot.send_message(message.chat.id,answer, reply_markup=constants.user_markup_start)
         log(message,answer)
 
-        bot.send_message(message.chat.id,answer)
-    else:
-        answer = "Работоспособность 2 кнопки"
-        bot.send_message(message.chat.id,answer)
-        log(message,answer)
+    elif message.text == "Выбрать день недели":
+        log(message,"Главное меню")
+        bot.send_message(message.chat.id, "Выберите день недели", reply_markup=constants.user_markup_days)
+
+    elif message.text == "Вернуться назад":
+        bot.send_message(message.chat.id, "Главное меню", reply_markup=constants.user_markup_start)
+
+    elif message.text == "Расписание звонков":
+        bot.send_message(message.from_user.id, "1 пара: 08:30 - 10:00\n"
+                                               "2 пара: 10:10 - 11:40\n"
+                                               "3 пара: 11:50 - 13:20\n"
+                                               "4 пара: 13:30 - 15:00\n"
+                                               "5 пара: 15:10 - 16:40\n",reply_markup=constants.user_markup_start)
 
 bot.polling(none_stop=True, interval=0)
