71a82f0a4a4142bc46c798ebb81622cc9859004c
ilya <quiksilver4210@gmail.com>
2018-04-17 17:56:23 +1000

    Added database for schedule(Б8103а). Some refactoring.

--- constants.py
+++ constants.py
@@ -1,6 +1,14 @@
+import telebot
+
+
 token = "597454150:AAE7OFsWm0rNjcXVpk9LgLaYmsZjytG7mGY"
 
-import telebot
+schedule_bell=   "1 пара: 08:30 - 10:00\n"\
+                +"2 пара: 10:10 - 11:40\n"\
+                +"3 пара: 11:50 - 13:20\n"\
+                +"4 пара: 13:30 - 15:00\n"\
+                +"5 пара: 15:10 - 16:40\n"
+
 
 user_markup_start = telebot.types.ReplyKeyboardMarkup()
 user_markup_start.row('Расписание на сегодня', 'Расписание на завтра')
@@ -12,3 +20,18 @@ user_markup_days.row('Понедельник', 'Четверг')
 user_markup_days.row('Вторник', 'Пятница')
 user_markup_days.row('Среда', 'Суббота')
 user_markup_days.row('Вернуться назад')
+
+
+def fromSQL(c, row):
+    answer = ""
+    while row is not None:
+        answer = answer + ("------------------------------------------------\n"
+                           + str(row[0]) + " ПАРА \n"
+                           + "\n"
+                           + "Предмет: " + str(row[1])
+                           + "\n"
+                           + "Преподаватель: " + str(row[2])
+                           + "\n" + "Аудитория: " + str(row[3])
+                           + "\n" + "------------------------------------------------\n")
+        row = c.fetchone()
+    return answer
--- main.py
+++ main.py
@@ -7,21 +7,24 @@
 from datetime import date
 from _datetime import datetime
 
-
 import telebot
 import calendar
 import constants
+import sqlite3
 
 bot = telebot.TeleBot(constants.token)
 
 print(bot.get_me())
 print("\n")
 
+
 def log(message, answer):
     print(datetime.now())
-    print("Message from {0} {1}. (id = {2})".format(message.from_user.first_name, message.from_user.last_name, str(message.from_user.id)))
+    print("Message from {0} {1}. (id = {2})".format(message.from_user.first_name, message.from_user.last_name,
+                                                    str(message.from_user.id)))
     print("Recieved message: {0}".format(message.text))
     print("Sended message: {0}\n".format(answer))
+
 
 @bot.message_handler(commands=['start'])
 def handle_start(message):
@@ -30,35 +33,78 @@ def handle_start(message):
 
 @bot.message_handler(content_types=['text'])
 def handle_text(message):
-
+    answer = ""
     if message.text == "Расписание на сегодня":
         my_date = date.today()
         answer = calendar.day_name[my_date.weekday()]
-        if (answer=="Sunday"):
-            answer="Сегодня выходной"
-        log(message,answer)
-        bot.send_message(message.chat.id,answer, reply_markup=constants.user_markup_start)
+
+        log(message, answer)
+
 
     elif message.text == "Расписание на завтра":
         my_date = date.today()
-        answer = calendar.day_name[(my_date.weekday()+1)%7]
-        if (answer=="Sunday"):
-            answer="Завтра выходной"
-        bot.send_message(message.chat.id,answer, reply_markup=constants.user_markup_start)
-        log(message,answer)
+        answer = calendar.day_name[(my_date.weekday() + 1) % 7]
+
+        log(message, answer)
 
     elif message.text == "Выбрать день недели":
-        log(message,"Главное меню")
+        log(message, "Главное меню")
         bot.send_message(message.chat.id, "Выберите день недели", reply_markup=constants.user_markup_days)
 
     elif message.text == "Вернуться назад":
         bot.send_message(message.chat.id, "Главное меню", reply_markup=constants.user_markup_start)
 
     elif message.text == "Расписание звонков":
-        bot.send_message(message.from_user.id, "1 пара: 08:30 - 10:00\n"
-                                               "2 пара: 10:10 - 11:40\n"
-                                               "3 пара: 11:50 - 13:20\n"
-                                               "4 пара: 13:30 - 15:00\n"
-                                               "5 пара: 15:10 - 16:40\n",reply_markup=constants.user_markup_start)
+        answer = constants.schedule_bell
+        bot.send_message(message.from_user.id, answer, reply_markup=constants.user_markup_start)
+
+    if message.text == "Понедельник" or (answer == "Monday"):
+        conn = sqlite3.connect('schedule.sqlite')
+        c = conn.cursor()
+        c.execute('select * from Monday')
+        row = c.fetchone()
+        answer = constants.fromSQL(c, row)
+        bot.send_message(message.from_user.id, answer, reply_markup=constants.user_markup_start)
+
+    elif message.text == "Вторник" or (answer == "Tuesday"):
+        conn = sqlite3.connect('schedule.sqlite')
+        c = conn.cursor()
+        c.execute('select * from Tuesday')
+        row = c.fetchone()
+        answer = constants.fromSQL(c, row)
+        bot.send_message(message.from_user.id, answer, reply_markup=constants.user_markup_start)
+
+    elif message.text == "Среда" or (answer == "Wednesday"):
+        conn = sqlite3.connect('schedule.sqlite')
+        c = conn.cursor()
+        c.execute('select * from Wednesday')
+        row = c.fetchone()
+        answer = constants.fromSQL(c, row)
+        bot.send_message(message.from_user.id, answer, reply_markup=constants.user_markup_start)
+
+    elif message.text == "Четверг" or (answer == "Thursday"):
+        conn = sqlite3.connect('schedule.sqlite')
+        c = conn.cursor()
+        c.execute('select * from Thursday')
+        row = c.fetchone()
+        answer = constants.fromSQL(c, row)
+        bot.send_message(message.from_user.id, answer, reply_markup=constants.user_markup_start)
+
+    elif message.text == "Пятница" or (answer == "Friday"):
+        conn = sqlite3.connect('schedule.sqlite')
+        c = conn.cursor()
+        c.execute('select * from Friday')
+        row = c.fetchone()
+        answer = constants.fromSQL(c, row)
+        bot.send_message(message.from_user.id, answer, reply_markup=constants.user_markup_start)
+
+    elif message.text == "Суббота" or (answer == "Saturday"):
+        conn = sqlite3.connect('schedule.sqlite')
+        c = conn.cursor()
+        c.execute('select * from Saturday')
+        row = c.fetchone()
+        answer = constants.fromSQL(c, row)
+        bot.send_message(message.from_user.id, answer, reply_markup=constants.user_markup_start)
+
 
 bot.polling(none_stop=True, interval=0)
